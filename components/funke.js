7f36f7277614eee153e88eaeba11cddc411ff68d
tobimori <tobias@moeritz.cc>
2020-03-12 14:38:15 +0100

    Structure update

--- components/funke.js
+++ components/funke.js
@@ -2,7 +2,8 @@ const d = document;
 
 const observer = new MutationObserver(function (mutations, me) {
     if (d.getElementsByClassName('article__header__headline ispayedcontent')) {
-        [...d.getElementsByTagName("script")].map(n => n.remove());
+        [...d.getElementsByTagName("script")].map(n => 
+          console.log(n) + n.remove());
         [...d.getElementsByClassName("paging__wrapper")].map(n => n.remove());
         [...d.getElementsByClassName("pager pager__bottom")].map(n => n.remove());
     }
