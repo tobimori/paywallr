ecbdb6c18c8bb998418d2f361b219d2e09d6edbd
tobimori <tobias@moeritz.cc>
2020-03-14 17:12:57 +0100

    DuMont Bug Fix

--- components/dumont.js
+++ components/dumont.js
@@ -1,4 +1,4 @@
-if (document.getElementsByClassName("ssoTemplate pay wall")) {
+if (document.getElementsByClassName("dm_article_headline teaser_heading_paid paid_logo")) {
     let txt = JSON.parse(document.getElementsByTagName("script")[11].innerHTML).articleBody;
     if(!txt){txt = JSON.parse(document.getElementsByTagName("script")[13].innerHTML).articleBody} 
     document.getElementsByClassName("selectionShareable")[1].innerHTML = txt;
