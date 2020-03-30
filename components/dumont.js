c0cb8b11757c462849d2f57932c425ca55b12319
tobimori <tobias@moeritz.cc>
2020-03-30 20:24:47 +0200

    dumont: changed abused bug

--- components/dumont.js
+++ components/dumont.js
@@ -8,9 +8,8 @@
 //  github.com/tobimori/unpaywall
 //
 
-if (document.getElementsByClassName("dm_article_headline teaser_heading_paid paid_logo")) {
-    let txt = JSON.parse(document.getElementsByTagName("script")[11].innerHTML).articleBody;
-    if (!txt) txt = JSON.parse(document.getElementsByTagName("script")[13].innerHTML).articleBody;
-    document.getElementsByClassName("selectionShareable")[1].innerHTML = txt;
+if (document.getElementsByClassName("content-behind-wall hide-paid-content")[0]) {
+    removeElements(document.getElementsByClassName("ssoTemplate pay wall"), document.getElementsByClassName("logo title backToLandingPage"));
     document.getElementById('c1-template-platzhalter').remove();
-}
+    document.getElementsByClassName("content-behind-wall hide-paid-content")[0].classList.remove("hide-paid-content")
+};
