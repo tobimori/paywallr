fdd89645d24a27e8b299a280fc468192df8f8b6b
tobimori <tobias@moeritz.cc>
2020-03-30 13:27:22 +0200

    Fixx for cross plattform

--- components/faz/useragent.js
+++ components/faz/useragent.js
@@ -14,11 +14,12 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
         console.log(details);
         var headers = details.requestHeaders;
         headers.forEach(function(header, i) {
-            if (header.name.toLowerCase() == 'user-agent') { 
-                header.value = 'Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/67.0.3396.87 Mobile Safari/537.36';
-            }
+            if(header.name.toLowerCase() == 'user-agent') header.value = 'Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/67.0.3396.87 Mobile Safari/537.36';
         });  
-        if(details.type = "xmlhttprequest") return {requestHeaders: headers};
+        if(
+            !details.originUrl && details.type == "xmlhttprequest" || 
+            details.initiator == "https://www.faz.net" && details.type == "xmlhttprequest"
+        ) return {requestHeaders: headers};
     },
     // Request filter
     {urls: ["*://m.faz.net/*"]},
