99eeb55bc5e20afe4054df0830cf14de170f3c55
tobimori <tobias@moeritz.cc>
2020-03-30 13:10:35 +0200

    Changed check to type only as originURL is empty in FF

--- components/faz/useragent.js
+++ components/faz/useragent.js
@@ -11,15 +11,14 @@
 chrome.webRequest.onBeforeSendHeaders.addListener(
     function(details) {
         // Replace the User-Agent header so FAZ doesn't redirect
+        console.log(details);
         var headers = details.requestHeaders;
         headers.forEach(function(header, i) {
             if (header.name.toLowerCase() == 'user-agent') { 
                 header.value = 'Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/67.0.3396.87 Mobile Safari/537.36';
             }
         });  
-        let t;
-        details.originUrl ? t = new URL(details.originUrl) : t = new URL(details.initiator);
-        if(t.hostname == "www.faz.net") return {requestHeaders: headers};
+        if(details.type = "xmlhttprequest") return {requestHeaders: headers};
     },
     // Request filter
     {urls: ["*://m.faz.net/*"]},
