b364d61e3c07faa1b38a8765789ed3e71a8982ea
tobimori <tobias@moeritz.cc>
2020-03-30 18:34:43 +0200

    faz: universal working check if article is paid content

--- components/faz/faz.js
+++ components/faz/faz.js
@@ -7,35 +7,38 @@
 //  faz component
 //  github.com/tobimori/unpaywall
 //
-
 const d = document;
+let isPaidContent; // check if article is paid content
+[...d.getElementsByTagName("script")].forEach(e => {
+    if(e.innerText == "\n    (window.isPaidContent = true);\n") isPaidContent = true
+});
+if (isPaidContent) { 
 
-const mReq = new XMLHttpRequest();
-const oUrl = new URL(window.location.href);
-const mUrl = new URL(oUrl.pathname, "https://m.faz.net/");
-
-const downloadDone = (e) => {
-    d.getElementsByClassName("atc-ReadTime_Text")[0].innerText = "Unlocked by Unpaywall";
-    d.getElementsByClassName("atc-TextParagraph")[0].innerText = JSON.parse(mReq.response.getElementById("schemaOrgJson").innerHTML).ArticleBody;
+    const mReq = new XMLHttpRequest();
+    const oUrl = new URL(window.location.href);
+    const mUrl = new URL(oUrl.pathname, "https://m.faz.net/");
+    
+    const downloadDone = (e) => {
+        d.getElementsByClassName("atc-ReadTime_Text")[0].innerText = "Unlocked by Unpaywall";
+        d.getElementsByClassName("atc-TextParagraph")[0].innerText = JSON.parse(mReq.response.getElementById("schemaOrgJson").innerHTML).ArticleBody;
+        console.log(window.isPaidContent);
+    };
+    
+    mReq.addEventListener("load", downloadDone);
+    mReq.open("GET", mUrl.href);
+    mReq.responseType = "document";
+    mReq.send();
+    
+    removeElements(
+        d.getElementsByClassName("js-atc-ContainerPaywall atc-ContainerPaywall"), // Paywall ad
+        d.getElementsByClassName("atc-ContainerInfo js-atc-ContainerInfo"), // Author information
+        d.getElementsByClassName("o-Icon atc-ReadTime_Icon"), // Reading time icon
+        d.getElementsByClassName("atc-TextParagraph") // paragraphs
+    );
+    
+    d.getElementsByClassName("atc-Text js-atc-Text")[0].innerHTML = '<p class="atc-TextParagraph">Unlocking...</p>'
+    d.getElementsByClassName("atc-ReadTime_Text")[0].innerText = "Unlocking..."
 };
-
-mReq.addEventListener("load", downloadDone);
-mReq.open("GET", mUrl.href);
-mReq.responseType = "document";
-// DONE: set User Agent using webRequest API for cross plattform
-// now in components/faz/useragent.js
-// mReq.setRequestHeader("User-Agent", "Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/67.0.3396.87 Mobile Safari/537.36");
-d.getElementsByClassName("js-atc-ContainerPaywall atc-ContainerPaywall") && mReq.send();
-removeElements(
-    d.getElementsByClassName("js-atc-ContainerPaywall atc-ContainerPaywall"), // Paywall ad
-    d.getElementsByClassName("atc-ContainerInfo js-atc-ContainerInfo"), // Author information
-    d.getElementsByClassName("o-Icon atc-ReadTime_Icon"), // Reading time icon
-    d.getElementsByClassName("atc-TextParagraph") // paragraphs
-);
-d.getElementsByClassName("atc-Text js-atc-Text")[0].innerHTML = '<p class="atc-TextParagraph">Unlocking...</p>'
-d.getElementsByClassName("atc-ReadTime_Text")[0].innerText = "Unlocking..."
-
-
 
 
 
