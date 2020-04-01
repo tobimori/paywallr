69edff008709eefd6d471d88db42ac3817611dec
tobimori <tobias@moeritz.cc>
2020-04-01 23:09:18 +0200

    how tf does this work

--- settingslib.js
+++ settingslib.js
@@ -7,3 +7,30 @@
 //  settings library
 //  github.com/tobimori/paywallr
 //
+
+const isSite = (url) => {
+    if (!url) return false;
+    a = new URL(url).hostname.toString().split('.');
+    if(a.length !== 2) a = a.slice(a.length - 2);
+    return sites.includes(a.join('.'));
+};
+
+const isSiteEnabled = (url) => {};
+
+// extapi.storage.sync.set();
+extapi.browserAction.onClicked.addListener((tab) => {
+    // disable the active tab
+    extapi.browserAction.disable(tab.id);
+    // requires the "tabs" or "activeTab" permission
+    
+});
+
+browser.tabs.onUpdated.addListener(() => 
+    browser.tabs.query({
+        active: true,
+        currentWindow: true
+    }).then((tab) => {
+        !isSite(tab[0].url) && browser.browserAction.disable(tab[0].id);
+        }
+    )
+);
