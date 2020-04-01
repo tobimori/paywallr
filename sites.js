6f3b6cd9d912595f3b20aa15c3a127a6b165d689
tobimori <tobias@moeritz.cc>
2020-04-01 20:57:02 +0200

    add sites.ks

---
+++ sites.js
@@ -0,0 +1,62 @@
+//                                 ____   
+//     ___  ___ ___ ___    _____ _/ / /___
+//    / _ \/ _ `/ // / |/|/ / _ `/ / / __/
+//   / .__/\_,_/\_, /|__,__/\_,_/_/_/_/   
+//  /_/        /___/                      
+//
+//  supported sites
+//  github.com/tobimori/paywallr
+//
+
+const sites = [
+    "*://*.allgemeine-zeitung.de/*",
+    "*://*.bergedorfer-zeitung.de/*",
+    "*://*.morgenpost.de/*",
+    "*://*.buerstaedter-zeitung.de/*",
+    "*://*.cellesche-zeitung.de/*",
+    "*://*.dorstenerzeitung.de/*",
+    "*://*.dnn.de/*",
+    "*://*.echo-online.de/*",
+    "*://*.faz.net/*",
+    "*://*.giessener-anzeiger.de/*",
+    "*://*.goettinger-tageblatt.de/*",
+    "*://*.halternerzeitung.de/*",
+    "*://*.abendblatt.de/*",
+    "*://*.haz.de/*",
+    "*://*.hellwegeranzeiger.de/*",
+    "*://*.hochheimer-zeitung.de/*",
+    "*://*.hofheimer-zeitung.de/*",
+    "*://*.ikz-online.de/*",
+    "*://*.kn-online.de/*",
+    "*://*.kreis-anzeiger.de/*",
+    "*://*.ksta.de/*",
+    "*://*.rundschau-online.de/*",
+    "*://*.lampertheimer-zeitung.de/*",
+    "*://*.lauterbacher-anzeiger.de/*",
+    "*://*.lvz.de/*",
+    "*://*.ln-online.de/*",
+    "*://*.main-spitze.de/*",
+    "*://*.mz-web.de/*",
+    "*://*.mittelhessen.de/*",
+    "*://*.maz-online.de/*",
+    "*://*.muensterlandzeitung.de/*",
+    "*://*.nahverkehrhamburg.de/*",
+    "*://*.neuepresse.de/*",
+    "*://*.nrwz.de/*",
+    "*://*.nrz.de/*",
+    "*://*.oberhessische-zeitung.de/*",
+    "*://*.ostsee-zeitung.de/*",
+    "*://*.otz.de/*",
+    "*://*.paz-online.de/*",
+    "*://*.ruhrnachrichten.de/*",
+    "*://*.saarbruecker-zeitung.de/*",
+    "*://*.sn-online.de/*",
+    "*://*.thueringer-allgemeine.de/*",
+    "*://*.usinger-anzeiger.de/*",
+    "*://*.waz.de/*",
+    "*://*.wp.de/*",
+    "*://*.wiesbadener-kurier.de/*",
+    "*://*.waz-online.de/*",
+    "*://*.wormser-zeitung.de/*",
+    "*://*.ze.tt/*"
+];
